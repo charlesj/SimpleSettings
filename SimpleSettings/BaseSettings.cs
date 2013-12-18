2843676c25e4b4eed4444ad71c75680a1bb090d6
Josh Charles <josh.charles@gmail.com>
2013-12-17 21:54:41 -0800

    added simple reflective settings as base settings

---
+++ SimpleSettings/BaseSettings.cs
@@ -0,0 +1,50 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="BaseSettings.cs" company="Josh Charles">
+//   Copyright 2013 Josh Charles.  Released under the MIT License.
+// </copyright>
+// <summary>
+//   This settings object uses reflection to load the settings from application configuration.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace SimpleSettings
+{
+	using System;
+
+	/// <summary>
+	/// This settings object uses reflection to load the settings from application configuration.
+	/// </summary>
+	public abstract class BaseSettings : Settings
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="BaseSettings"/> class.
+		/// </summary>
+		protected BaseSettings()
+		{
+			this.LoadSettingsUsingReflection();
+		}
+
+		/// <summary>
+		/// Sets the values of the properties of this object with the values from the application configuration file.
+		/// </summary>
+		protected void LoadSettingsUsingReflection()
+		{
+			var properties = this.GetProperties();
+			foreach (var property in properties)
+			{
+				try
+				{
+					var settingsValue = this.GetValue(property.Name);
+					var propertyType = property.PropertyType;
+					var typedValue = this.TypeConverter.Convert(settingsValue, propertyType);
+					property.SetValue(this, typedValue);
+				}
+				catch (Exception exception)
+				{
+					string message = string.Format("Error trying to set a value for {0} from application configuration.  See inner exception for more details.", property.Name);
+					throw new Exception(message, exception);
+				}
+			}
+		}
+	}
+}
